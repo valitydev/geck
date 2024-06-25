33ecd3e8aefb4e7f1e8a989594220ad46d097552
Anatoly Karlov <karleowne@gmail.com>
2024-06-25 10:12:52 +0700

    ThriftType fix

--- serializer/src/main/java/dev/vality/geck/serializer/kit/tbase/ThriftType.java
+++ serializer/src/main/java/dev/vality/geck/serializer/kit/tbase/ThriftType.java
@@ -1,31 +1,33 @@
 package dev.vality.geck.serializer.kit.tbase;
 
+import org.apache.thrift.meta_data.EnumMetaData;
 import org.apache.thrift.meta_data.FieldValueMetaData;
 import org.apache.thrift.protocol.TType;
 
 public enum ThriftType {
 
-    NULL(-2),
-    BOOLEAN(TType.BOOL),
-    BYTE(TType.BYTE),
-    DOUBLE(TType.DOUBLE),
-    SHORT(TType.I16),
-    INTEGER(TType.I32),
-    LONG(TType.I64),
-    STRING(TType.STRING),
-    ENUM(TType.ENUM),
-    // если в гек сунуть трифт uuid все сломается , тк старый энам пересекается с новым uuid
-    OLD_ENUM(TType.UUID),
-    LIST(TType.LIST),
-    SET(TType.SET),
-    MAP(TType.MAP),
-    STRUCT(TType.STRUCT),
-    BINARY(21);
+    BOOLEAN(TType.BOOL, 17),
+    BYTE(TType.BYTE, 17),
+    DOUBLE(TType.DOUBLE, 17),
+    SHORT(TType.I16, 17),
+    INTEGER(TType.I32, 17),
+    LONG(TType.I64, 17),
+    STRING(TType.STRING, 17),
+    ENUM(TType.ENUM, 17),
+    OLD_ENUM((byte) 16, 9),
+    UUID(TType.UUID, 17),
+    LIST(TType.LIST, 17),
+    SET(TType.SET, 17),
+    MAP(TType.MAP, 17),
+    STRUCT(TType.STRUCT, 17),
+    BINARY(21, 17);
 
-    int code;
+    private final int code;
+    private final int ignoredThriftVersion;
 
-    ThriftType(int code) {
+    ThriftType(int code, int ignoredThriftVersion) {
         this.code = code;
+        this.ignoredThriftVersion = ignoredThriftVersion;
     }
 
     public int getCode() {
@@ -44,6 +46,14 @@ public enum ThriftType {
     public static ThriftType findByMetaData(FieldValueMetaData valueMetaData) {
         if (valueMetaData.isBinary()) {
             return ThriftType.BINARY;
+        }
+        if (valueMetaData instanceof EnumMetaData) {
+            if (valueMetaData.getType() == ThriftType.ENUM.getCode()) {
+                return ThriftType.ENUM;
+            }
+            if (valueMetaData.getType() == ThriftType.OLD_ENUM.getCode()) {
+                return ThriftType.OLD_ENUM;
+            }
         }
         return ThriftType.findByCode(valueMetaData.getType());
     }
